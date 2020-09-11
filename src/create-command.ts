b03fce920f40640d1e2a438f88a13ab2dc8c918a
Sergey Ukustov <sergey@ukstv.me>
2020-09-11 19:18:21 +0400

    Create and link, finally

--- src/create-command.ts
+++ src/create-command.ts
@@ -3,32 +3,62 @@ import IdentityWallet from "identity-wallet";
 import { LocalFilecoinProvider } from "@ukstv/local-filecoin-provider";
 import { AccountID } from "caip";
 import * as blockchainUtils from "@ukstv/3id-blockchain-utils";
+import { schemasList, publishSchemas } from "@ceramicstudio/idx-schemas";
+import { IDX } from "@ukstv/ceramicstudio-idx";
 
 export async function createCommand(
   seed: string,
   privateKey: string,
   network: string
 ) {
-  const client = new CeramicClient();
-  try {
-    const identityWallet = await IdentityWallet.create({
-      getPermission: async (rec) => (rec.payload.paths as unknown) as string[],
-      useThreeIdProv: true,
-      seed: seed,
-      ceramic: client,
-    });
+  const ceramic = new CeramicClient();
+  const identityWallet = await IdentityWallet.create({
+    getPermission: async (rec) => (rec.payload.paths as unknown) as string[],
+    useThreeIdProv: true,
+    seed: seed,
+    ceramic: ceramic,
+  });
+  await ceramic.setDIDProvider(identityWallet.getDidProvider());
+  const schemas = await publishSchemas({ ceramic, schemas: schemasList });
+  const idx = new IDX({ ceramic, schemas });
+  await idx.authenticate();
+  const accountLinksDefinition = await idx.createDefinition({
+    name: "Account Links",
+    schema: schemas.CryptoAccountLinks,
+  });
 
+  try {
     const isTestnet = network !== "f";
     const provider = new LocalFilecoinProvider(privateKey, isTestnet);
     const address = (await provider.getAccounts())[0];
     const caipNetwork = isTestnet ? "fil:t" : "fil:f";
     const account = new AccountID(`${address}@${caipNetwork}`);
     const did = identityWallet.DID;
-    const proof = await blockchainUtils.createLink(did, account, provider);
-    await identityWallet.idx.load();
-    await identityWallet.idx.addAccountLink(proof);
+    const linkProof = await blockchainUtils.createLink(did, account, provider);
+    const accountLinkDocument = await ceramic.createDocument(
+      "account-link",
+      { metadata: { owners: [linkProof.account] } },
+      { applyOnly: true }
+    );
+    await accountLinkDocument.change({ content: linkProof });
+    await idx.set(accountLinksDefinition, {
+      [linkProof.account]: accountLinkDocument.id,
+    });
+    const idxMapped = await idx.getIDXDocID(idx.did.id);
+    if (!idxMapped) {
+      console.log("idx is not linked to DID. Linking idx...");
+      const docId = idx.did.id.replace(/^did:3:/, "ceramic://");
+      const rootIndexId = (await idx._rootIndex._getOrCreateOwnDoc()).id;
+      const threeIdDocument = await ceramic.loadDocument(docId);
+      await threeIdDocument.change({
+        content: {
+          ...threeIdDocument.content,
+          idx: rootIndexId,
+        },
+      });
+    }
     console.log(`Linked ${address} to ${did}`);
   } finally {
-    await client.close();
+    await ceramic.close();
   }
 }
--- src/list-command.ts
+++ src/list-command.ts
@@ -1,12 +1,40 @@
 import CeramicClient from "@ceramicnetwork/ceramic-http-client";
-import { CDefs, ThreeIDX } from "identity-wallet";
+import { IDX } from "@ukstv/ceramicstudio-idx";
+import * as _ from 'lodash'
+
+const schemas: Record<string, string> = {
+  BasicProfile:
+    "ceramic://bagcqceram3zvgd4y6ggl3bkoprtlmrvsorsdnkg3e2g4rd5xywqwv3wcaj4q",
+  Definition:
+    "ceramic://bagcqcerau4zp2ulb45mem3khalj35dtlmknsy2jvt4kmdqp7ihvy7nv46asa",
+  DocIdDocIdMap:
+    "ceramic://bagcqceral72u2ghww67eqtw22pk2hjxb2iwglqegccguqf6yhrnivrm7k3va",
+  DocIdMap:
+    "ceramic://bagcqceraub7atazkr7rgibvkulcu47otimzaejqdlft4bl6ietyhmx2svwma",
+  RootIndex:
+    "ceramic://bagcqcera4xpocatz5wuftjkwwvraafaniywaey5vkhkvvas3fhqc5i4nx2pa",
+  StringMap:
+    "ceramic://bagcqcera7gva3wbetgzncdwvbni4vd37bc62huevcmvyezzcvwkchvw36mwa",
+  CryptoAccountLinks:
+    "ceramic://bagcqcera4wduhtkgxuqg73wxowlieykt6iwtlldfwzug3brpfuqlu2x4yazq",
+};
+
+const AccountLinksDefinition =
+  "ceramic://bagcqcerab36ia3na5kpeyh4mfazrc2gjb7d4a6p2m3rcniyzuo2z4ep3wjoa";
 
 export async function listCommand(did: string) {
-  const client = new CeramicClient();
-  const idx = new ThreeIDX(client);
-  await idx.load(did);
-  const accountLinksCatalogue = await idx.docs[CDefs.accountLinks];
-  const state = accountLinksCatalogue?.content || {};
-  console.log(state);
-  await client.close();
+  const ceramic = new CeramicClient();
+  const idx = new IDX({ ceramic, schemas });
+  const accountLinkDocuments = await idx.get<Record<string, string>>(AccountLinksDefinition, did)
+  let accounts: string[] = []
+  for (let account in accountLinkDocuments) {
+    const docId = accountLinkDocuments[account]
+    const document = await ceramic.loadDocument(docId)
+    if (_.isString(document.content) && document.content === did) {
+      accounts.push(account)
+    }
+  }
+  console.log(`Linked to ${did}:`)
+  console.log(accounts)
+  await ceramic.close();
 }
